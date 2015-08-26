044a1ae1a1f31f016ed6de5c08c93b7c693c0b32
Byron Ruth <b@devel.io>
2015-08-26 08:59:16 -0400

    Add i2b2 ETL conventions document
    Documents are now encapsulated in a class instance to manage their
    respective states.
    
    Fix #14
    
    Signed-off-by: Byron Ruth <b@devel.io>

--- services/etl/main.py
+++ services/etl/main.py
@@ -8,13 +8,6 @@ from flask import Flask, Response
 from parser import Document
 
 
-app = Flask(__name__)
-
-# Document API URL
-DOCUMENT_URL = 'https://api.github.com/repos/PEDSnet/Data_Models/contents/PEDSnet/V2/docs/Pedsnet_CDM_V2_OMOPV5_ETL_Conventions.md'  # noqa
-COMMITS_URL = 'https://api.github.com/repos/PEDSnet/Data_Models/commits'
-FILE_PATH = 'PEDSnet/V2/docs/Pedsnet_CDM_V2_OMOPV5_ETL_Conventions.md'  # noqa
-
 # Required mediatype for the Accept header to get the raw content.
 GITHUB_RAW_MEDIATYPE = 'application/vnd.github.v3.raw'
 GITHUB_DEFAULT_MEDIATYPE = 'application/vnd.github.v3'
@@ -22,95 +15,115 @@ GITHUB_DEFAULT_MEDIATYPE = 'application/vnd.github.v3'
 # Token for GitHub authorization. Set on startup.
 GITHUB_AUTH_TOKEN = None
 
-content_last_modified = None
-content_etag = None
-
-commit_last_modified = None
-commit_etag = None
+DEFAULT_TIMEOUT = 5
 
 
-def parse_model():
-    global content_etag, content_last_modified
+class ETLConventionsResource():
+    def __init__(self, document_url, commits_url, file_path):
+        self.document_url = document_url
+        self.commits_url = commits_url
+        self.file_path = file_path
 
-    headers = {
-        'Accept': GITHUB_RAW_MEDIATYPE,
-        'Authorization': 'token ' + GITHUB_AUTH_TOKEN,
-    }
+        self.content_last_modified = None
+        self.content_etag = None
 
-    if content_last_modified:
-        headers['If-Modified-Since'] = content_last_modified
+        self.commit_last_modified = None
+        self.commit_etag = None
 
-    if content_etag:
-        headers['If-None-Match'] = content_etag
+    def __call__(self):
+        "Entrypoint for Flask routing."
+        try:
+            model = self.parse_model()
+        except HTTPError:
+            return 503, ''
 
-    resp = requests.get(DOCUMENT_URL,
-                        headers=headers,
-                        timeout=5)
+        try:
+            commit = self.parse_commit()
+        except HTTPError:
+            return 503, ''
 
-    resp.raise_for_status()
+        resp = Response(json.dumps({
+            'commit': commit,
+            'model': model,
+        }))
 
-    content_last_modified = resp.headers['Last-Modified']
-    content_etag = resp.headers['ETag']
+        resp.headers['Content-Type'] = 'application/json'
 
-    # Wrap decoded bytes in file-like object.
-    buff = io.StringIO(resp.text)
+        return resp
 
-    return Document(buff).parse()
+    def parse_model(self):
+        headers = {
+            'Accept': GITHUB_RAW_MEDIATYPE,
+            'Authorization': 'token ' + GITHUB_AUTH_TOKEN,
+        }
+
+        if self.content_last_modified:
+            headers['If-Modified-Since'] = self.content_last_modified
+
+        if self.content_etag:
+            headers['If-None-Match'] = self.content_etag
+
+        resp = requests.get(self.document_url,
+                            headers=headers,
+                            timeout=DEFAULT_TIMEOUT)
+
+        resp.raise_for_status()
+
+        self.content_last_modified = resp.headers['Last-Modified']
+        self.content_etag = resp.headers['ETag']
+
+        # Wrap decoded bytes in file-like object.
+        buff = io.StringIO(resp.text)
+
+        return Document(buff).parse()
+
+    def parse_commit(self):
+        headers = {
+            'Accept': GITHUB_DEFAULT_MEDIATYPE,
+            'Authorization': 'token ' + GITHUB_AUTH_TOKEN,
+        }
+
+        if self.commit_last_modified:
+            headers['If-Modified-Since'] = self.commit_last_modified
+
+        if self.commit_etag:
+            headers['If-None-Match'] = self.commit_etag
+
+        resp = requests.get(self.commits_url,
+                            params={'path': self.file_path},
+                            headers=headers,
+                            timeout=DEFAULT_TIMEOUT)
+
+        resp.raise_for_status()
+
+        self.commit_last_modified = resp.headers['Last-Modified']
+        self.commit_etag = resp.headers['ETag']
+
+        # Get the most recent commit.
+        commit = resp.json()[0]
+
+        return {
+            'sha': commit['sha'],
+            'date': commit['commit']['committer']['date']
+        }
 
 
-def parse_commit():
-    global commit_etag, commit_last_modified
+pedsnet_v2 = ETLConventionsResource(
+        document_url='https://api.github.com/repos/PEDSnet/Data_Models/contents/PEDSnet/V2/docs/Pedsnet_CDM_V2_OMOPV5_ETL_Conventions.md',  # noqa
+        commits_url='https://api.github.com/repos/PEDSnet/Data_Models/commits',  # noqa
+        file_path='PEDSnet/V2/docs/Pedsnet_CDM_V2_OMOPV5_ETL_Conventions.md')
 
-    headers = {
-        'Accept': GITHUB_DEFAULT_MEDIATYPE,
-        'Authorization': 'token ' + GITHUB_AUTH_TOKEN,
-    }
-
-    if commit_last_modified:
-        headers['If-Modified-Since'] = commit_last_modified
-
-    if commit_etag:
-        headers['If-None-Match'] = commit_etag
-
-    resp = requests.get(COMMITS_URL,
-                        params={'path': FILE_PATH},
-                        headers=headers,
-                        timeout=5)
-
-    resp.raise_for_status()
-
-    commit_last_modified = resp.headers['Last-Modified']
-    commit_etag = resp.headers['ETag']
-
-    # Get the most recent commit.
-    commit = resp.json()[0]
-
-    return {
-        'sha': commit['sha'],
-        'date': commit['commit']['committer']['date']
-    }
+i2b2_v2 = ETLConventionsResource(
+        document_url='https://api.github.com/repos/PEDSnet/Data_Models/contents/i2b2/V2/docs/i2b2_pedsnet_v2_etl_conventions.md',  # noqa
+        commits_url='https://api.github.com/repos/PEDSnet/Data_Models/commits',  # noqa
+        file_path='i2b2/V2/docs/i2b2_pedsnet_v2_etl_conventions.md')
 
 
-@app.route('/', methods=['GET'])
-def index():
-    try:
-        model = parse_model()
-    except HTTPError:
-        return 503, ''
+# Initialize the flask app and register the routes.
+app = Flask(__name__)
 
-    try:
-        commit = parse_commit()
-    except HTTPError:
-        return 503, ''
-
-    resp = Response(json.dumps({
-        'commit': commit,
-        'model': model,
-    }))
-
-    resp.headers['Content-Type'] = 'application/json'
-
-    return resp
+app.add_url_rule('/pedsnet/2.0.0', 'pedsnet_v2', pedsnet_v2, methods=['GET'])
+app.add_url_rule('/i2b2/2.0.0', 'i2b2_v2', i2b2_v2, methods=['GET'])
 
 
 if __name__ == '__main__':
