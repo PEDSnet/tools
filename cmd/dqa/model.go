906e5cf3f92328af55d9437eb5ba9e43aedd142c
Byron Ruth <b@devel.io>
2016-04-08 12:56:50 -0400

    Use DMS client default service URL

--- cmd/dqa/model.go
+++ cmd/dqa/model.go
@@ -6,8 +6,6 @@ import (
 	"strings"
 )
 
-const DefaultServiceURL = "http://data-models-service.research.chop.edu"
-
 var ErrInvalidHeader = errors.New("invalid results header")
 
 // Rank is an ordered enumeration of result issue rankings.
